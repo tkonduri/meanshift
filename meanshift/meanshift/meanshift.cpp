15abe61a788b02fe23efec70b493b7755773a3ad
Tejaswi Konduri <tkonduri@uncc.edu>
2017-04-08 03:31:42 -0400

    Mean shift algo implementation basic version.. - Implemented till getting the average of points inside the kernel of all the points from the image.   - Implemented the kernel part of the algorithm, where we check all the points surrounding the point in a square fashion.   - TODO: Implement this operation itteratively till the mean shift vector is constant... - TODO: Update the average value of each point into the original vector again.

--- meanshift/meanshift/meanshift.cpp
+++ meanshift/meanshift/meanshift.cpp
@@ -4,6 +4,11 @@
 #include <string>
 #include <vector>
 
+//Also known as Kernel..
+//2 for Dataset 1
+//4.5 for Dataset 2
+#define SQUARE_DIM 2
+
 using namespace std;
 
 string ipFilePath;
@@ -47,9 +52,62 @@ void displayCoord(vector<Coordinates> vtNew)
 	for (it = vtNew.begin(); it != vtNew.end(); it++)
 	{
 		cout << "x = " << it->x;
-		cout << "y = " << it->y;
-		cout << "z = " << it->z;
-		cout << "bit = " << it->bit<<endl;
+		cout << ", y = " << it->y;
+		cout << ", z = " << it->z;
+		cout << ", bit = " << it->bit<<endl;
+	}
+}
+
+void averageFunc(vector<Coordinates> vtNew)
+{
+	float mX = 0., mY = 0., mZ = 0.;
+	vector<Coordinates> ::iterator it;
+	for (it = vtNew.begin(); it != vtNew.end(); it++)
+	{		
+		mX += it->x;
+		mY += it->y;
+		mZ += it->z;
+	}
+	mX /= vtNew.size();
+	mY /= vtNew.size();
+	mZ /= vtNew.size();
+
+	cout << mX << "," << mY << "," << mZ;
+	//TODO: Add this new point in the main vector...
+}
+
+void msAlgo(vector<Coordinates> vtNew)
+{
+	//New variable declarations...
+	vector<Coordinates> vtInRange;
+	vector<Coordinates> ::iterator it, it2;
+
+	//For each point in the image, check for all the points inside its surrounding square to find out the mean.
+	for (it = vtNew.begin(); it != vtNew.end(); it++)	
+	{
+		float startPntX = ((it->x) - SQUARE_DIM);
+		float startPntY = ((it->y) + SQUARE_DIM);
+		float endPntX = ((it->x) + SQUARE_DIM);
+		float endPntY = ((it->y) - SQUARE_DIM);
+		
+		for (float i = startPntY; i >= endPntY; i--)
+			for (float j = startPntX; j <= endPntX; j++)
+			{				
+				//TODO: Check if we need to handle border cases...
+				if (i>0 && j>0)
+				{
+					//Traversing each entry of the points vector to see if there is a match (If the point is present)...
+					for (it2 = vtNew.begin(); it2 != vtNew.end(); it2++)
+					{						
+						if (i == (it2->y) && j == (it2->x))
+						{
+							//Adding the the element in the range to the vtInRange vector, for average calculation...
+							addCoord(vtInRange, j, i, it2->z);
+						}
+					}					
+				}
+			}
+		averageFunc(vtInRange);
 	}
 }
 
@@ -66,6 +124,9 @@ int main(int argc, char **argv)
 	//Function calls
 	readIpFile(vt);
 	displayCoord(vt);
+
+	cout << "##### Algorithm:" << endl;
+	msAlgo(vt);
 
 	return 0;
 }
